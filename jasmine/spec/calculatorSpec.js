86037dad39135e8cde42033035c1cbaf29929c74
Milan Grubnić <MilanG@aol.com>
2016-01-29 22:27:38 -0600

    styling

--- jasmine/spec/calculatorSpec.js
+++ jasmine/spec/calculatorSpec.js
@@ -4,20 +4,23 @@ describe('Calculator', function() {
         Calculator.current = 0;
     });
 
-    it('should store the current value at all times', function() {
-        expect(Calculator.current).toBeDefined();
-        expect(Calculator.current).toEqual(0);
+    describe('when adding numbers', function() {
+
+        it('should store the current value at all times', function() {
+            expect(Calculator.current).toBeDefined();
+            expect(Calculator.current).toEqual(0);
+        });
+
+        it('should add numbers', function() {
+            expect(Calculator.add(5)).toEqual(5);
+            expect(Calculator.add(5)).toEqual(10);
+        });
+
+        it('should add any number of numbers', function() {
+            expect(Calculator.add(1, 2, 3)).toEqual(6);
+            expect(Calculator.add(1, 2, 3, 4)).toEqual(16);
+        });
+
     });
-
-
-    it('should add numbers', function() {
-        expect(Calculator.add(5)).toEqual(5);
-        expect(Calculator.add(5)).toEqual(10);
-    });
-
-    it('should add any number of numbers', function() {
-        expect(Calculator.add(1, 2, 3)).toEqual(6);
-        expect(Calculator.add(1, 2, 3, 4)).toEqual(16);
-    });
-
+    
 });
